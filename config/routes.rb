dbbd355db6970187f8e757ca68483fbd78df206e
Josette Garcia <josettetgarcia@gmail.com>
2022-09-11 16:06:29 -0500

    updates routes again

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,9 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     post "/signup", to: "users#create"
     get "/me", to: "users#show"
-  
+  end
 
     get "*path", 
     to: "fallback#index", 
     constraints: ->(req) { !req.xhr? && req.format.html? }
-  end
 end
